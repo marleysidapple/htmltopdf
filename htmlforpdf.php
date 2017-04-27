c869c5ed481488ad202d4700dabac4ed49909f36
marleysidapple <bsiddhartha25@gmail.com>
2017-04-27 14:47:49 +0800

    first push

---
+++ htmlforpdf.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Invoice</title>
+<style>
+body { 
+	font-family:Arial, Helvetica, sans-serif;
+	font-size:18px;
+	margin:0px;
+	background:#f5f5f5;
+}
+h1,h2,h3,h4,h5,h6 { 
+	margin:0px;
+	padding:0px; 
+}
+
+h5 { 
+	font-size:20px;
+	margin:8px 0;
+}
+
+p { 
+	margin:0px;
+	padding:0px;
+	line-height:22px; 
+}
+#wrapper { 
+	width:720px;
+	margin:0 auto;
+}
+.container { 
+	width:100%;
+	float:left;
+}
+.mailaddress { 
+	font-size:20px;
+	margin-bottom:8px;
+}
+</style>
+</head>
+
+<body>
+
+<div id="wrapper">
+<div class="container">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td width="23%"><img src="logo.jpg" border="0" width="100" alt="" /></td>
+    <td rowspan="3" align="right" valign="bottom">
+    <h2>The Lawncare Man</h2>
+<p class="mailaddress">www.lawncareman.com.au</p>
+      <p>PO Box 822</p>
+      <p>SCARBOROUGH WA 6922</p>
+      <p>ABN 37-963-396-323</p>
+      <p>Tel.: (08) 9337 1300 Fax (08) 9337 1303</p>
+      <p>Email: accounts@lawncareman.com.au</p></td>
+  </tr>
+  
+  <tr>
+    <td height="28" width="40%"><h4>TAX INVOICE NUMBER 384</h4></td>
+  </tr>
+  <tr>
+    <td height="26"></td>
+    </tr>
+  <tr>
+    <td colspan="3"><hr style="height:2px; background:#000; border:none;" /></td>
+  </tr>
+  <tr>
+    <td colspan="3" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table width="60%" style="margin:20px auto 20px;" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td width="3%" align="left" valign="top"><strong>TO:</strong></td>
+            <td>&nbsp;</td>
+            <td valign="top">
+            
+
+              <p> David Banks </p>
+			  <p>5 Ferndale Corner<br> Mindarie, WA  6030               </p>
+              </td>
+            <td width="18%">&nbsp;</td>
+          </tr>
+                  </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td colspan="3"><table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin-top:10px;">
+      	<tr>
+        <td width="75%" height="20">Square Meterage 80 (m2)</td>
+        <td width="6%">&nbsp;</td>
+        <td style="text-align:right;">&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="75%" height="20">Dollar Spot Fungus</td>
+        <td width="6%">&nbsp;</td>
+        <td style="text-align:right;">$106.00</td>
+        <td>&nbsp;</td>
+      </tr>
+  
+            <tr>
+        <td height="20">Add Good and Services Tax (GST) @10.0%</td>
+        <td>&nbsp;</td>
+        <td style="text-align:right;">$10.60</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td colspan="4" height="5">&nbsp;</td>
+      </tr>
+      <tr>
+        <td colspan="4">Product will take 20-25 days for full effect</td>
+      </tr>
+                         <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td width="10%"><hr style="height:2px; background:#000; border:none;" /></td>
+        <td width="35%">&nbsp;</td>
+       </tr>
+      <tr>
+        <td height="37" align="right" style="font-weight: bold; font-size: 21px;">Total Amount :</td>
+        <td>&nbsp;</td>
+        <td style="text-align: right; font-weight: bold; font-size: 21px;">$116.60</td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td colspan="3"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="13%" height="23">Client No.:</td>
+        <td width="10%">15296</td>
+        <td width="57%" align="center">For Payment within 7 days please</td>
+        <td align="right">26 Apr 2017</td>
+        </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td colspan="3"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td height="28">Direct Deposit Details:</td>
+        <td>BSB 066122</td>
+        <td>A/C 1014 0201</td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="30" colspan="3" align="center"><h4>Please quote client and invoice number</h4></td>
+  </tr>
+  <tr>
+    <td colspan="3"><hr style="border:dashed 2px #000;" /></td>
+  </tr>
+  <tr>
+    <td colspan="3">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="3"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="32%"><h3>REMITTANCE ADVICE</h3></td>
+        <td width="16%">&nbsp;</td>
+        <td width="28%">&nbsp;</td>
+        <td width="24%"><h3>INVOICE No. 384</h3></td>
+      </tr>
+      <tr>
+        <td valign="top"><p>The Lawncare Man</p>
+          <p>PO Box 822</p>
+          <p>SCARBOROUGH WA 6922</p></td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td valign="top"><p>26 Apr 2017</p>
+          <p>$116.60</p>
+          <p>15296</p>
+          <p>David Banks</p></td>
+      </tr>
+    </table></td>
+  </tr>
+  <!--<tr>
+    <td colspan="3">&nbsp;</td>
+  </tr>-->
+</table>
+
+</div>
+</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// include autoloader
+require_once 'dompdf/autoload.inc.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+
+// instantiate and use the dompdf class
+$html = file_get_contents("htmlforpdf.php");
+$dompdf->loadHtml($html);
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF (1 = download and 0 = preview)
+$dompdf->stream("codexworld",array("Attachment"=>0));
+
+?>
